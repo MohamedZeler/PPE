04cb957a1fabfc3ed6cbc0338594a5d893e963c2
Dronis <florian.nicol@hotmail.fr>
2014-03-27 17:37:12 +0100

    1ere requete C PARTE

--- soka/soka/frm_AjouterEntreprise.Designer.vb
+++ soka/soka/frm_AjouterEntreprise.Designer.vb
@@ -23,8 +23,22 @@ Partial Class frm_AjouterEntreprise
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txt_designation = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txt_adresse = New System.Windows.Forms.TextBox()
+        Me.txt_cpentreprise = New System.Windows.Forms.TextBox()
+        Me.txt_villeentreprise = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txt_telentreprise = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txt_mailentreprise = New System.Windows.Forms.TextBox()
+        Me.cbo_paysentreprise = New System.Windows.Forms.ComboBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
@@ -37,12 +51,12 @@ Partial Class frm_AjouterEntreprise
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Nouvelle entreprise"
         '
-        'TextBox1
+        'txt_designation
         '
-        Me.TextBox1.Location = New System.Drawing.Point(90, 68)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 1
+        Me.txt_designation.Location = New System.Drawing.Point(84, 68)
+        Me.txt_designation.Name = "txt_designation"
+        Me.txt_designation.Size = New System.Drawing.Size(100, 20)
+        Me.txt_designation.TabIndex = 1
         '
         'Label2
         '
@@ -53,21 +67,155 @@ Partial Class frm_AjouterEntreprise
         Me.Label2.TabIndex = 2
         Me.Label2.Text = "Désignation : "
         '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(21, 34)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Adresse :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(2, 60)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(70, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Code Postal :"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(40, 86)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(32, 13)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Ville :"
+        '
+        'txt_adresse
+        '
+        Me.txt_adresse.Location = New System.Drawing.Point(76, 31)
+        Me.txt_adresse.Name = "txt_adresse"
+        Me.txt_adresse.Size = New System.Drawing.Size(100, 20)
+        Me.txt_adresse.TabIndex = 6
+        '
+        'txt_cpentreprise
+        '
+        Me.txt_cpentreprise.Location = New System.Drawing.Point(76, 57)
+        Me.txt_cpentreprise.Name = "txt_cpentreprise"
+        Me.txt_cpentreprise.Size = New System.Drawing.Size(100, 20)
+        Me.txt_cpentreprise.TabIndex = 7
+        '
+        'txt_villeentreprise
+        '
+        Me.txt_villeentreprise.Location = New System.Drawing.Point(76, 83)
+        Me.txt_villeentreprise.Name = "txt_villeentreprise"
+        Me.txt_villeentreprise.Size = New System.Drawing.Size(100, 20)
+        Me.txt_villeentreprise.TabIndex = 8
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txt_villeentreprise)
+        Me.GroupBox1.Controls.Add(Me.txt_cpentreprise)
+        Me.GroupBox1.Controls.Add(Me.txt_adresse)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Location = New System.Drawing.Point(15, 94)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(188, 126)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Localisation"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(23, 229)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(64, 13)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Téléphone :"
+        '
+        'txt_telentreprise
+        '
+        Me.txt_telentreprise.Location = New System.Drawing.Point(91, 226)
+        Me.txt_telentreprise.Name = "txt_telentreprise"
+        Me.txt_telentreprise.Size = New System.Drawing.Size(100, 20)
+        Me.txt_telentreprise.TabIndex = 11
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(53, 255)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(32, 13)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Mail :"
+        '
+        'txt_mailentreprise
+        '
+        Me.txt_mailentreprise.Location = New System.Drawing.Point(91, 252)
+        Me.txt_mailentreprise.Name = "txt_mailentreprise"
+        Me.txt_mailentreprise.Size = New System.Drawing.Size(100, 20)
+        Me.txt_mailentreprise.TabIndex = 13
+        '
+        'cbo_paysentreprise
+        '
+        Me.cbo_paysentreprise.FormattingEnabled = True
+        Me.cbo_paysentreprise.Location = New System.Drawing.Point(91, 288)
+        Me.cbo_paysentreprise.Name = "cbo_paysentreprise"
+        Me.cbo_paysentreprise.Size = New System.Drawing.Size(139, 21)
+        Me.cbo_paysentreprise.TabIndex = 14
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(49, 291)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(36, 13)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "Pays :"
+        '
         'frm_AjouterEntreprise
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(325, 290)
+        Me.ClientSize = New System.Drawing.Size(325, 343)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.cbo_paysentreprise)
+        Me.Controls.Add(Me.txt_mailentreprise)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txt_telentreprise)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txt_designation)
         Me.Controls.Add(Me.Label1)
         Me.Name = "frm_AjouterEntreprise"
         Me.Text = "frm_AjouterEntreprise"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents txt_designation As System.Windows.Forms.TextBox
     Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txt_adresse As System.Windows.Forms.TextBox
+    Friend WithEvents txt_cpentreprise As System.Windows.Forms.TextBox
+    Friend WithEvents txt_villeentreprise As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents txt_telentreprise As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents txt_mailentreprise As System.Windows.Forms.TextBox
+    Friend WithEvents cbo_paysentreprise As System.Windows.Forms.ComboBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
 End Class
--- soka/soka/frm_AjouterEntreprise.vb
+++ soka/soka/frm_AjouterEntreprise.vb
@@ -1,3 +1,32 @@
-﻿Public Class frm_AjouterEntreprise
+﻿Imports Microsoft.VisualBasic
+Imports System.Data.SqlClient
 
+Public Class frm_AjouterEntreprise
+
+
+    Private Sub frm_AjouterEntreprise_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim cnn As SqlConnection
+
+        cnn = New SqlConnection("DataSource=172.30.0.115;InitialCatalog=SOKA_GestionCo;UserID=SIO1;Password=SIO1MDP")
+
+        cnn.Open()
+
+        Dim cmd As SqlCommand
+        Dim JeuEnr As SqlDataReader
+
+        cmd = New SqlCommand()
+        cmd.Connection = cnn
+        cmd.CommandText = "SELECT * FROM PAYS"
+
+        JeuEnr = cmd.ExecuteReader()
+        MsgBox(JeuEnr.GetValue(1))
+
+        While JeuEnr.Read()
+            cbo_paysentreprise.Items.Add(JeuEnr.GetValue(1))
+        End While
+
+        JeuEnr.Close()
+
+    End Sub
 End Class
