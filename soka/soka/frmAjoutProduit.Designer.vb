b5e636c4f2d1450d51412ae49b5715c3f2c19bb9
Herr-Nest <fonteneau.adrien@gmail.com>
2014-03-27 17:42:23 +0100

    ajout d'un produit
    Debut formulaire ajout produit

---
+++ soka/soka/frmAjoutProduit.Designer.vb
@@ -0,0 +1,116 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAjoutProduit
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNomModele = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPrixBase = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtMatiere = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtNomModele
+        '
+        Me.txtNomModele.Location = New System.Drawing.Point(104, 80)
+        Me.txtNomModele.Name = "txtNomModele"
+        Me.txtNomModele.Size = New System.Drawing.Size(128, 20)
+        Me.txtNomModele.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(25, 83)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Nom Modele :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(41, 122)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Prix Base :"
+        '
+        'txtPrixBase
+        '
+        Me.txtPrixBase.Location = New System.Drawing.Point(104, 119)
+        Me.txtPrixBase.Name = "txtPrixBase"
+        Me.txtPrixBase.Size = New System.Drawing.Size(39, 20)
+        Me.txtPrixBase.TabIndex = 0
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(149, 122)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(13, 13)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "€"
+        '
+        'txtMatiere
+        '
+        Me.txtMatiere.Location = New System.Drawing.Point(104, 155)
+        Me.txtMatiere.Name = "txtMatiere"
+        Me.txtMatiere.Size = New System.Drawing.Size(128, 20)
+        Me.txtMatiere.TabIndex = 0
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(50, 162)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(48, 13)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Matiere :"
+        '
+        'frmAjoutProduit
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(352, 331)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPrixBase)
+        Me.Controls.Add(Me.txtMatiere)
+        Me.Controls.Add(Me.txtNomModele)
+        Me.Name = "frmAjoutProduit"
+        Me.Text = "frmAjoutProduit"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtNomModele As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtPrixBase As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtMatiere As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+End Class
---
+++ soka/soka/frmAjoutProduit.vb
@@ -0,0 +1,7 @@
+﻿Public Class frmAjoutProduit
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtMatiere.TextChanged
+
+    End Sub
+
+End Class
