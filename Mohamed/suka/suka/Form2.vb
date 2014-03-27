fd8d80d262129f2734c1550247ffd54ab184b9d3
MohamedZeler <m-boumediene@live.fr>
2014-03-27 14:39:37 +0100

    FIchiers de base
    Mes fichiers de base.

---
+++ Mohamed/suka/suka/Form2.vb
@@ -0,0 +1,32 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data.SqlClient
+Public Class Form2
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        Dim cnn As SqlConnection
+        Dim cmd As SqlCommand
+        Dim adapter As SqlDataAdapter
+        Dim table As New DataTable
+
+        cnn = New SqlConnection("Data Source=172.30.0.115;Initial Catalog=SOKA_GestionCo;User ID=SIO1;Password=SIO1MDP")
+
+        cnn.Open()
+
+        cmd = New SqlCommand()
+        cmd.Connection = cnn
+        cmd.CommandText = "Select nomParcours, typeEmbarcation, prix FROM TARIF T, EMBARCATION E, PARCOURS P WHERE E.idEmbarcation = T.idEmbarcation AND T.idParcours = P.idParcours ORDER BY 1,2"
+
+
+        adapter = New SqlDataAdapter()
+        adapter.SelectCommand = cmd
+
+        table.Locale = System.Globalization.CultureInfo.InvariantCulture
+        adapter.Fill(table)
+
+        cnn.Close()
+
+        dgvTarifs.AutoGenerateColumns = True
+        dgvTarifs.DataSource = table
+        dgvTarifs.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders
+    End Sub
+End Class
---
+++ Mohamed/suka/suka/FrmListeContacts.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmListeContacts
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.AutoEllipsis = True
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(245, 102)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Liste de contacts"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'FrmListeContacts
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(269, 126)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.Name = "FrmListeContacts"
+        Me.Text = "Liste de contacts"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
