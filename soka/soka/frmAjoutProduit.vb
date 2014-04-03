75e4db19fb96d6fcad838fb4137c1df3dead2068
Herr-Nest <fonteneau.adrien@gmail.com>
2014-04-03 17:45:16 +0200

    frm catalogue

--- soka/soka/frmAjoutProduit.vb
+++ soka/soka/frmAjoutProduit.vb
@@ -3,12 +3,21 @@ Imports System.Data.SqlClient
 Public Class frmAjoutProduit
     Dim cnn As SqlConnection
     Private Sub frmAjoutProduit_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim cmd As SqlCommand
-        Dim jeuEnr As SqlDataReader
-
+        
         cnn = New SqlConnection("Data Source=172.30.0.115;Initial Catalog=SOKA_GestionCo;User ID=SIO1;Password=SIO1MDP")
         cnn.Open()
+        Dim cmd As SqlCommand
+        Dim jeuEnr As SqlDataReader
         cmd = New SqlCommand()
+        cmd.Connection = cnn
+        cmd.CommandText = "SELECT IdCollection, IntituleCollection FROM COLECTION"
+        jeuEnr = cmd.ExecuteReader()
+        While jeuEnr.Read
+            cboCollection.Items.Add(jeuEnr(1))
+        End While
+
+
+
         
 
 
